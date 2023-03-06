87978b3483522cc55ced9cc7482e086de9b060b3
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-03-06 20:58:38 +0000

    Made 3 components

---
+++ portfolio/src/App.js
@@ -0,0 +1,19 @@
+//import logo from './logo.svg';
+import './css/style.css';
+import './css/styles2.css';
+import NavBar from './Nav';
+import Header from './Header';
+import Bodycon from './Bodycon';
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Header />
+      <Bodycon />
+    </div>
+  );
+}
+
+
+
+export default App;
