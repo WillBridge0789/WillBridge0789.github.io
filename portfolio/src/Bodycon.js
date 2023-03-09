4d50109754031d546a93518b5b932883631ee308
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-03-09 22:14:30 +0000

    Refactor

--- portfolio/src/Bodycon.js
+++ portfolio/src/Bodycon.js
@@ -1,17 +1,17 @@
 function Bodycon() {
-    return (
-        <div className="container text-center">
+  return (
+    <div className="container text-center">
       <div className="row align-items-start">
-          <div className="col-6 col-4">
-            <img src="/img/desk-setup.jpg" className="rounded m-15 p-3 float-start" alt="Desk setup" />
-          </div>
-          <div className="col-6 col-4">
-            <p className="lead main-para m-15 p-3"><strong>L</strong>orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br />
+        <div className="col-6 col-4">
+          <img src="/img/desk-setup.jpg" className="rounded m-15 p-3 float-start" alt="Desk setup" />
+        </div>
+        <div className="col-6 col-4">
+          <p className="lead main-para m-15 p-3"><strong>L</strong>orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br />
             <a href="/portfolio/pages/blog.html">Click here to see Blog</a></p>
-          </div>
+        </div>
       </div>
-      </div>
-    )
+    </div>
+  )
 }
 
 export default Bodycon;
--- portfolio/src/Header.js
+++ portfolio/src/Header.js
@@ -1,15 +1,15 @@
 function Header() {
-    return (
-        <div className="container-fluid">
-        <div className="row">
-      <div className="col">
-        <header id="main-header">
-          <h1 className="head-title">Welcome to Will's Portfolio Landing Page</h1>
-        </header>
-      </div>
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col">
+          <header id="main-header">
+            <h1 className="head-title">Welcome to Will's Portfolio Landing Page</h1>
+          </header>
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default Header;
