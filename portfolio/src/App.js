5bf978fafe3ee6ed1b09e09ed3b0dd52e6bfd335
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-03-10 15:00:39 +0000

    Changed styling

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,6 @@
 //import logo from './logo.svg';
 import './css/style.css';
-import './css/styles2.css';
+//import './css/styles2.css';
 import NavBar from './Nav';
 import Header from './Header';
 import Bodycon from './Bodycon';
