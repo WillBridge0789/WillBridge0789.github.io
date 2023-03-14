1cf1e2ca96afd71e217a603caaff0db2e45b993e
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-03-14 19:33:24 +0000

    Bootstrap cards to put screenshots of projects

--- portfolio/src/Bodycon2.js
+++ portfolio/src/Bodycon2.js
@@ -2,12 +2,61 @@ function Bodycon2() {
     return (
         <div className="container d-flex justify-content-start text-center">
             <div className="row align-items-start">
-                <div className="col-6 mt-5">
-                    <p id="image-block">Picture Here</p>
+                <div className="col-4 mt-4">
+                    <div className="card">
+                        <img src="..." class="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-6 mt-5">
-                    <p className="lead main-para p-5"><strong>L</strong>orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br />
-                        <a href="/portfolio/pages/blog.html">Click here to see my Projects</a></p>
+                <div className="col-4 mt-4">
+                    <div className="card">
+                        <img src="..." className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                    </div>
+                </div>
+                <div className="col-4 mt-4">
+                    <div className="card">
+                        <img src="..." className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row align-items-start">
+                <div className="col-4 mt-4">
+                    <div className="card">
+                        <img src="..." class="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-4 mt-4">
+                    <div className="card">
+                        <img src="..." className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                    </div>
+                </div>
+                <div className="col-4 mt-4">
+                    <div className="card">
+                        <img src="..." className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                    </div>
                 </div>
             </div>
         </div>
