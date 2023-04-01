d8c4c7ef6adf45282c55d30e091d4c93201f68e6
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-04-01 14:43:26 +0000

    Portfolio live

--- portfolio/src/Contact.js
+++ portfolio/src/Contact.js
@@ -12,7 +12,7 @@ function Contact() {
                             <p className="mb-5" id="contact_p">For any questions, job inquiries, etc., feel free to contact me!</p>
                             <ul className="list-unstyled pl-md-5 mb-5">
                                 <li className="d-flex mb-2">
-                                    859.519.8213
+                                    
                                 </li>
                                 <li className="d-flex mb-2">
                                     bj.burbridge@yahoo.com
