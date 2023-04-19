3cfe34f270494cc81c9de21157e2271108526914
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-04-19 01:42:35 +0000

    Color changes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,13 +2,24 @@
 import './css/style.css';
 //import './css/styles2.css';
 import Home from './Home';
+import React, { useState } from 'react';
 
 
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleClick = () => {
+    setDarkMode(!darkMode);
+  };
   return (
     <div>
       <Home />
+      <div className={darkMode ? 'dark' : ''}>
+        <button className="dark-mode" onClick={handleClick}>
+          Dark Mode
+        </button>
+      </div>
     </div>
   );
 }
--- portfolio/src/blogs/Week3.js
+++ portfolio/src/blogs/Week3.js
@@ -1,16 +1,18 @@
 function Week3() {
     return (
-        <div class="card" id="week3">
-            <h4 class="card-header">Sprint 1: Some Reflection</h4>
-            <div class="card-body">
-                <h5 class="card-title">1. What is the number one thing that held you back this week? Why?</h5>
-                <p class="card-text">Maybe I'm holding myself back. When I am working on things, sometimes I worry about my surroundings which tend to take my attention off of what I need to do. I live with 2 other people after all. One of them being my partner.</p>
-                <h5 class="card-title">2. What is one thing you would like to learn more about regarding JavaScript (JSON, Data Structures), Bootstrap, or Atomic Design?</h5>
-                <p class="card-text">a. I want to understand and learn functions and about structuring the data types correctly.<br /> AND <br /> b. Get a more solidified foundation on bootstrap frameworks and its principles</p>
-                <h5 class="card-title">3. How does In-Person Bootcamp compare to Remote Onboarding?</h5>
-                <p class="card-text">I feel like in-person bootcamp would make classmates and work a bit more engaging compared to remote.</p>
-                <h5 class="card-title">4. What is your “Why”?</h5>
-                <p class="card-text">In the past, I've always put people before myself. One of the main reasons "why" is because I'm doing this for MYSELF for once. My partner and the kid comes second. He knows, understands and fully supports me while doing this. I am going to try my hardest not to let myself down...and I wont let him and the kid down either.</p>
+        <div className="container">
+            <div class="card" id="week3">
+                <h4 class="card-header">Sprint 1: Some Reflection</h4>
+                <div class="card-body">
+                    <h5 class="card-title">1. What is the number one thing that held you back this week? Why?</h5>
+                    <p class="card-text">Maybe I'm holding myself back. When I am working on things, sometimes I worry about my surroundings which tend to take my attention off of what I need to do. I live with 2 other people after all. One of them being my partner.</p>
+                    <h5 class="card-title">2. What is one thing you would like to learn more about regarding JavaScript (JSON, Data Structures), Bootstrap, or Atomic Design?</h5>
+                    <p class="card-text">a. I want to understand and learn functions and about structuring the data types correctly.<br /> AND <br /> b. Get a more solidified foundation on bootstrap frameworks and its principles</p>
+                    <h5 class="card-title">3. How does In-Person Bootcamp compare to Remote Onboarding?</h5>
+                    <p class="card-text">I feel like in-person bootcamp would make classmates and work a bit more engaging compared to remote.</p>
+                    <h5 class="card-title">4. What is your “Why”?</h5>
+                    <p class="card-text">In the past, I've always put people before myself. One of the main reasons "why" is because I'm doing this for MYSELF for once. My partner and the kid comes second. He knows, understands and fully supports me while doing this. I am going to try my hardest not to let myself down...and I wont let him and the kid down either.</p>
+                </div>
             </div>
         </div>
     )
